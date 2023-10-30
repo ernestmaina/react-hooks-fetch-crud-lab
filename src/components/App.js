8d730f37eebdbca21521d552c8a37f193849885c
ErnestMaina <you@example.com>
2023-10-30 18:29:13 +0300

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
-import React, { useState } from "react";
+// App.js
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]); 
+  
+  const onDeleteQuestion = (questionId) => {
+   
+   
+    fetch(`http://localhost:4000/questions/${questionId}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+     
+        setQuestions((prevQuestions) =>
+          prevQuestions.filter((question) => question.id !== questionId)
+        );
+      })
+      .catch((error) => {
+        console.error("Error deleting question: ", error);
+      });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching questions: ", error);
+      });
+  }, []); 
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : (
+        <QuestionList questions={questions} onDeleteQuestion={onDeleteQuestion} />
+      )}
     </main>
   );
 }
